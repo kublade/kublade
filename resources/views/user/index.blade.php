93851b9cd8fb6f37cfffecc960504b1e52ab5fa0
Marcel Menk <marcel.menk@vmmx.io>
2025-06-25 21:26:51 +0200

    fix: table pagination alignment

--- resources/views/user/index.blade.php
+++ resources/views/user/index.blade.php
@@ -11,29 +11,31 @@
                         <i class="bi bi-plus"></i>
                     </a>
                 </div>
-                <div class="card-body p-0">
+                <div class="card-body d-flex flex-column gap-4 p-0">
                     <table class="table">
                         <thead class="font-monospace">
                             <tr>
-                                <th>{{ __('Name') }}</th>
-                                <th>{{ __('Email') }}</th>
-                                <th>{{ __('Roles') }}</th>
-                                <th>{{ __('Actions') }}</th>
+                                <th class="w-100">{{ __('Name') }}</th>
+                                <th scope="col">{{ __('Email') }}</th>
+                                <th scope="col">{{ __('Roles') }}</th>
+                                <th scope="col">{{ __('Actions') }}</th>
                             </tr>
                         </thead>
                         <tbody>
                             @foreach ($users as $user)
                                 <tr>
-                                    <td>{{ $user->name }}</td>
+                                    <td class="w-100">{{ $user->name }}</td>
                                     <td>{{ $user->email }}</td>
                                     <td>{{ $user->roles->pluck('name')->implode(', ') }}</td>
                                     <td>
-                                        <a href="{{ route('user.update', $user->id) }}" class="btn btn-sm btn-warning" title="{{ __('Update') }}">
-                                            <i class="bi bi-pencil"></i>
-                                        </a>
-                                        <a href="{{ route('user.delete.action', $user->id) }}" class="btn btn-sm btn-danger{{ Auth::id() === $user->id ? ' disabled' : '' }}" title="{{ __('Delete') }}">
-                                            <i class="bi bi-trash"></i>
-                                        </a> 
+                                        <div class="d-flex gap-2">
+                                            <a href="{{ route('user.update', $user->id) }}" class="btn btn-sm btn-warning" title="{{ __('Update') }}">
+                                                <i class="bi bi-pencil"></i>
+                                            </a>
+                                            <a href="{{ route('user.delete.action', $user->id) }}" class="btn btn-sm btn-danger{{ Auth::id() === $user->id ? ' disabled' : '' }}" title="{{ __('Delete') }}">
+                                                <i class="bi bi-trash"></i>
+                                            </a> 
+                                        </div>
                                     </td>
                                 </tr>
                             @endforeach
